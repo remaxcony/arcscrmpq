6e02fbb981d7825d72948d52c4919bd05eb610a3
Remax Cony <kevin.espino@remax.es>
2024-09-30 13:24:34 +0100

    Commit

---
+++ data.js
@@ -0,0 +1,527 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-entrada",
+      "name": "ENTRADA",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 2.829641789061366,
+        "pitch": 0.07915060073509395,
+        "fov": 1.2910342431011943
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.094487302580145,
+          "pitch": 0.02994663268403741,
+          "rotation": 0,
+          "target": "1-pasillo-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-pasillo-1",
+      "name": "PASILLO 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 2.667876965357488,
+        "pitch": 0.10458139338741113,
+        "fov": 1.2910342431011943
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.11532412469015,
+          "pitch": 0.0324091148952661,
+          "rotation": 0,
+          "target": "2-pasillo-2"
+        },
+        {
+          "yaw": -1.6349373092495867,
+          "pitch": 0.0025137762168458977,
+          "rotation": 0,
+          "target": "0-entrada"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.159790910190777,
+          "pitch": 0.19955691395563946,
+          "title": "PLANTA BAJA",
+          "text": "Mira todas las fotos de este inmueble en el link del anuncio:&nbsp;https://www.idealista.com/pro/remaxconyoverseas/inmueble/104768246/<br>"
+        }
+      ]
+    },
+    {
+      "id": "2-pasillo-2",
+      "name": "PASILLO 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -2.2110441930992852,
+        "pitch": -0.07420915259359973,
+        "fov": 1.2910342431011943
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.8670558820797236,
+          "pitch": -0.07567613120390959,
+          "rotation": 0.7853981633974483,
+          "target": "3-hab-1"
+        },
+        {
+          "yaw": -2.71316873313941,
+          "pitch": -0.10110971661640811,
+          "rotation": 0.7853981633974483,
+          "target": "4-hab-2"
+        },
+        {
+          "yaw": -2.8464657262853628,
+          "pitch": -0.010238386081720563,
+          "rotation": 0,
+          "target": "5-salon"
+        },
+        {
+          "yaw": 1.82323124554099,
+          "pitch": 0.00807566696298423,
+          "rotation": 0,
+          "target": "1-pasillo-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-hab-1",
+      "name": "HAB 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -0.29251538059579296,
+        "pitch": -0.11727037727833789,
+        "fov": 1.2910342431011943
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.914337715404594,
+          "pitch": -0.05201525201231405,
+          "rotation": 4.71238898038469,
+          "target": "4-hab-2"
+        },
+        {
+          "yaw": 3.0797713952334753,
+          "pitch": 0.011667072304916815,
+          "rotation": 0,
+          "target": "2-pasillo-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-hab-2",
+      "name": "HAB 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 0.5515412613323711,
+        "pitch": -0.17762398014088632,
+        "fov": 1.2910342431011943
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.9550041848367599,
+          "pitch": -0.00819537626787259,
+          "rotation": 0.7853981633974483,
+          "target": "3-hab-1"
+        },
+        {
+          "yaw": -2.557551833041476,
+          "pitch": 0.0292343188043791,
+          "rotation": 0,
+          "target": "6-terraza"
+        },
+        {
+          "yaw": 2.48944801400501,
+          "pitch": 0.06437825311709489,
+          "rotation": 4.71238898038469,
+          "target": "2-pasillo-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-salon",
+      "name": "SALON",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -2.143294775891764,
+        "pitch": -0.02518707311407198,
+        "fov": 1.2910342431011943
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.516317368529613,
+          "pitch": 0.04712429975597843,
+          "rotation": 4.71238898038469,
+          "target": "6-terraza"
+        },
+        {
+          "yaw": 2.584827199112456,
+          "pitch": 0.006284318782473264,
+          "rotation": 0,
+          "target": "2-pasillo-2"
+        },
+        {
+          "yaw": 1.3504956561236252,
+          "pitch": -0.06953164190396777,
+          "rotation": 0,
+          "target": "7-pasillo-3"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-terraza",
+      "name": "TERRAZA",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -0.8581461101529086,
+        "pitch": -0.0542030515130385,
+        "fov": 1.2910342431011943
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.2173183349181187,
+          "pitch": -0.06438862803795686,
+          "rotation": 0,
+          "target": "5-salon"
+        },
+        {
+          "yaw": 2.8830246355980247,
+          "pitch": -0.040993151287532115,
+          "rotation": 0,
+          "target": "2-pasillo-2"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.7427974405340496,
+          "pitch": -0.4738710162432316,
+          "title": "AZOTEA",
+          "text": "Mira todas las fotos de este inmueble en el link del anuncio:&nbsp;https://www.idealista.com/pro/remaxconyoverseas/inmueble/104768246/"
+        }
+      ]
+    },
+    {
+      "id": "7-pasillo-3",
+      "name": "PASILLO 3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -0.3321190326079666,
+        "pitch": -0.07442521801961988,
+        "fov": 1.2910342431011943
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.1975785961537042,
+          "pitch": -0.004110819235311425,
+          "rotation": 1.5707963267948966,
+          "target": "8-hab-3"
+        },
+        {
+          "yaw": 0.07395195518768993,
+          "pitch": -0.06461900822550604,
+          "rotation": 0.7853981633974483,
+          "target": "9-cocina"
+        },
+        {
+          "yaw": -0.020847501509551947,
+          "pitch": -0.17997039960321182,
+          "rotation": 0,
+          "target": "10-bao"
+        },
+        {
+          "yaw": -1.863689491493144,
+          "pitch": 0.03411796473642248,
+          "rotation": 0,
+          "target": "5-salon"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-hab-3",
+      "name": "HAB 3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -0.0781675897220282,
+        "pitch": -0.008794511192792953,
+        "fov": 1.2910342431011943
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.8618169621183718,
+          "pitch": 0.07090062103503669,
+          "rotation": 0,
+          "target": "7-pasillo-3"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "9-cocina",
+      "name": "COCINA",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -0.42061607802802037,
+        "pitch": -0.007035608954229389,
+        "fov": 1.2910342431011943
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.005561225338666,
+          "pitch": 0.00820195219136366,
+          "rotation": 4.71238898038469,
+          "target": "7-pasillo-3"
+        },
+        {
+          "yaw": -2.1765922258925503,
+          "pitch": -0.07819090409046936,
+          "rotation": 1.5707963267948966,
+          "target": "10-bao"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "10-bao",
+      "name": "BAÑO",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 0.5049874447120963,
+        "pitch": -0.012312315669898766,
+        "fov": 1.2910342431011943
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.4888997858111948,
+          "pitch": 0.050072427767473116,
+          "rotation": 0,
+          "target": "7-pasillo-3"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": false
+  }
+};
